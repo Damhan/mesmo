03829a14d177f278029d3e14b1d8fd3ba4d9e617
Damhan Richardson <damhanrichardson@gmail.com>
2019-07-15 21:14:07 +0100

    Setup React

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import NavBar from './components/NavBar.js'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavLink,
+    NavToggle
+} from 'react-bootstrap'
+
+class NavBar extends Component {
+
+    render() {
+        return (
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">mesmo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+                <Nav.Link href="#signup">Sign Up</Nav.Link>
+                <Nav.Link href="#login">Log in</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        )
+    }
+
+}
+
+export default NavBar;
