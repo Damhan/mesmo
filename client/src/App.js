7c52d2bedb04514845ed6f116427a92e4506d219
Damhan Richardson <damhanrichardson@Gmail.com>
2019-07-17 23:59:01 +0100

    Progress on sign up modal

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 
 import NavBar from './components/NavBar.js';
 import UserList from './components/UserList';
+import SignUpModal from './components/SignUpModal';
 
 import {Provider} from 'react-redux';
 import store from './store'
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="wrapper">
         <NavBar />
+        <Container>
+          <SignUpModal/>
+        </Container>
         <UserList />
       </div>
     </Provider>
--- client/src/components/signupModal.js
+++ client/src/components/signupModal.js
@@ -2,22 +2,75 @@ import React, {component} from 'react';
 import {
     Button,
     Modal,
-    ModalHeader,
-    ModalBody,
     Form,
     FormGroup,
-    Label,
-    Input,
-} from 'react-boostrap';
+} from 'react-bootstrap'
 import {connect} from 'react-redux';
 import {addUser} from '../actions/userActions';
 
-class signupModal extends Component {
+class signupModal extends React.Component {
     state= {
         modal:false,
         email:'',
         password:''
     }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+
+    onChange = e => {
+        this.setState({[e.target.email] : e.target.value , [e.target.password] : e.target.password})
+    }
+
+    render() {
+        return (
+            <div>
+                <Button
+                    color="dark"
+                    style={{marginBottom:'2rem'}}
+                    onClick={this.toggle}>
+                    Add User
+                </Button>
+
+            <Modal
+                isOpen={this.state.modal}
+                toggle={this.toggle}>
+
+                <Modal.Header 
+                    toggle={this.toggle}>
+                    Add to Users
+
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={this.onSubmit}>
+                        <FormGroup>
+                            <label for="email">email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                id="email"
+                                placeholders="Add User Item"
+                                onChange={this.onChange}/>
+
+                            <input 
+                                type="text"
+                                name="pass"
+                                id="pass"
+                                placeholders="password"
+                                onChange={this.onChange}/>
+                        </FormGroup>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+
+            </div>
+        )
+    }
 }
+
 
 export default connect()(signupModal)
